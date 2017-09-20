f4ed025f65b93a12c81dc52350f862a36fde66d9
Shinobu Okano <okanoshinobu8@gmail.com>
2017-09-20 23:34:09 +0900

    🐙

--- app/src/main/java/com/os/operando/takosan/MainActivity.kt
+++ app/src/main/java/com/os/operando/takosan/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.os.operando.takosan
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
